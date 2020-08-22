87ac1928bff3b4fa0b0e0847591b4575439078b8
Kenneth Nwachukwu <nwachukwuken92@gmail.com>
2020-08-22 23:21:09 +0100

    server update

--- server.js
+++ server.js
@@ -19,7 +19,12 @@ app.use('/auth', require('./routes/auth'));
 app.use('/incomes', require('./routes/income'));
 app.use('/expenses', require('./routes/expense'));
 
-
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) =>
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    );
+  }
 
 
 
